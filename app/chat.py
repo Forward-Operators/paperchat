fb4e7af76469a5697dafb0c07dc9400fb2647891
Mateusz Kozak <mateusz@mkozak.pl>
2023-05-03 21:23:12 +0200

    cleanup chat.py

--- app/chat.py
+++ app/chat.py
@@ -1,30 +1,25 @@
 import logging
+import os
 
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import ChatOpenAI
-from app.tools.factory import get_database
+from app.tools.factory import get_database, get_embeddings
 
 db = get_database()
+embeddings = get_embeddings()
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 
 def ask(query):
-    # pdf_qa = RetrievalQAWithSourcesChain.from_chain_type(
-    #     llm=ChatOpenAI(temperature=0, model_name=os.getenv("OPENAI_GPT_MODEL")),
-    #     retriever=db.as_retriever(),
-    #     chain_type="map_reduce",
-    #     return_source_documents=True,
-    # )
-    # llm = ChatOpenAI(temperature=0, model_name=os.environ.get("OPENAI_GPT_MODEL"))
-    # chain = load_qa_chain(llm, chain_type="stuff")
-    docs = db.similarity_search(query=query, k=5)
-    print(docs)
-    result = docs
-    # result = chain.run(input_documents=docs, question=query)
-    # chat_history = []
-    # result = pdf_qa({"question": query, "chat_history": chat_history})
+    pdf_qa = RetrievalQAWithSourcesChain.from_chain_type(
+        llm=ChatOpenAI(temperature=0, model_name=os.getenv("OPENAI_GPT_MODEL")),
+        retriever=db.as_retriever(),
+        chain_type="refine",
+        return_source_documents=True,
+    )
+    result = pdf_qa({"question": query})
     return result
 
 
