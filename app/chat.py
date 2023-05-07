8de90d9de06cc70bb671cd8b19f13ec0a24ad43d
Mateusz Kozak <mateusz@mkozak.pl>
2023-05-07 19:43:42 +0200

    switch to ConversationalRetrievalChain and use prompt templates

--- app/chat.py
+++ app/chat.py
@@ -1,10 +1,12 @@
 import logging
 import os
 
-from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import ChatOpenAI
 from app.tools.factory import get_database, get_embeddings
+from app.templates.qa_prompt import QA_PROMPT
+from app.templates.condense_prompt import CONDENSE_PROMPT
 
 db = get_database()
 embeddings = get_embeddings()
@@ -12,15 +14,20 @@ embeddings = get_embeddings()
 # logging.basicConfig(level=logging.DEBUG)
 
 
-def ask(query):
-    pdf_qa = RetrievalQAWithSourcesChain.from_chain_type(
-        llm=ChatOpenAI(temperature=0, model_name=os.getenv("OPENAI_GPT_MODEL")),
-        retriever=db.as_retriever(),
-        chain_type="refine",
-        return_source_documents=True,
-    )
-    result = pdf_qa({"question": query})
-    return result
+def ask():
+    # pdf_qa = RetrievalQAWithSourcesChain.from_chain_type(
+    #     llm=ChatOpenAI(temperature=0, model_name=os.getenv("OPENAI_GPT_MODEL")),
+    #     retriever=db.as_retriever(),
+    #     chain_type="refine",
+    #     return_source_documents=True,
+    # )
+    # result = pdf_qa({"question": query})
+    model = ChatOpenAI(model_name=os.getenv("OPENAI_GPT_MODEL"), temperature=0, streaming=True)
+    retriever = db.as_retriever(search_kwargs={
+        "k": 5},  qa_template=QA_PROMPT, question_generator_template=CONDENSE_PROMPT)
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=model, retriever=retriever, return_source_documents=True)
+    return qa
 
 
 if __name__ == "__main__":
