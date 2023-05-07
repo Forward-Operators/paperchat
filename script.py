11acb31d14a794d29f9289ff48c8a238ce3a9403
Mateusz Kozak <mateusz@mkozak.pl>
2023-05-07 23:44:15 +0200

    fix path for dataset for ingester

---
+++ script.py
@@ -0,0 +1,43 @@
+import logging
+import os
+
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyMuPDFLoader
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter, SpacyTextSplitter
+from app.tools.factory import get_embeddings, get_database
+
+# logging.basicConfig(level=logging.DEBUG)
+
+embeddings = get_embeddings()
+db = get_database()
+
+
+def load_data():
+    docs_path = "/mnt/dataset/arxiv/pdf"
+    for filename in os.listdir(docs_path):
+        f = os.path.join(docs_path, filename)
+        if os.path.isfile(f):
+            try:
+                loader = PyMuPDFLoader(f)
+                print(f"Loading {f}")
+                text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size=2000, chunk_overlap=100, length_function=len,
+                )
+                documents = loader.load_and_split(text_splitter=text_splitter)
+                # print(documents)
+                db.add_documents(documents)
+            except Exception as e:
+                print(e)
+                continue
+
+
+def questions(query):
+    chain = RetrievalQAWithSourcesChain.from_chain_type(ChatOpenAI(temperature=0, model_name=os.getenv("OPENAI_GPT_MODEL")), chain_type="stuff", retriever=db.as_retriever())
+    result = chain({"question": query})
+    return result
+
+
+if __name__ == "__main__":
+    load_data()
