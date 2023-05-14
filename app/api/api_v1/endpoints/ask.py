62be608d55396f1c15ec89cb47b77263163d8c0a
Mateusz Kozak <mateusz@mkozak.pl>
2023-05-14 16:43:21 +0200

    accept json object as input

--- app/api/api_v1/endpoints/ask.py
+++ app/api/api_v1/endpoints/ask.py
@@ -5,10 +5,13 @@ from fastapi import APIRouter, Body, HTTPException, status
 from pydantic import BaseModel
 from resources.arxiv import chat
 
+class Query(BaseModel):
+    question: str
+
 router = APIRouter()
 
-
 @router.post("/ask")
-async def ingest(query: str = Body(...)):
-    answer = chat(query)
+async def ask(query: Query):
+    print(query.question)
+    answer = chat(query.question)
     return answer
